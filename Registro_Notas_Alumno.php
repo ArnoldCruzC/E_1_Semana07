00f3481d20518565c79ba51748a87c75bac827e6
Arnold Cruz <arnold_x100pre_10@hotmail.com>
2018-05-03 22:50:25 -0500

    Registro Notas

---
+++ Registro_Notas_Alumno.php
@@ -0,0 +1,58 @@
+<html>
+
+    <head>       
+        <meta charset="utf-8"> 
+        <title>Registro de Notas de Alumnos</title>
+    </head>
+    
+<h1 aling="center">Registro de Notas </h1>    
+<body>
+
+
+
+<p>
+
+<label for="nombre">Nombre: </label>
+
+<input type="text" name="nombre" id="nombre">
+
+<br/> <br/>
+
+<label for="apellidos">Apellidos: </label>
+
+<input type="text" name="apellido" id="apellido">
+
+<br/> <br/>
+
+<label for="email">Curso: </label>
+
+<input type="text" name="curso" id="email">
+
+<br/> <br/>
+
+<input type="radio" name="sexo" id="varon" value="Varón">
+
+<label for="Varon">Varón: </label>
+
+<br/> <br/>
+
+<input type="radio" name="sexo" id="mujer" value="Mujer">
+
+<label for="Mujer">Mujer: </label>
+
+<br/> <br/>
+
+<label for="Nota">Nota: </label>
+
+<input type="text" name="nota" id="nombre">
+
+<br/> <br/>
+
+
+<input type="submit" value="Enviar">
+
+</p>
+
+</body>
+
+</html>
