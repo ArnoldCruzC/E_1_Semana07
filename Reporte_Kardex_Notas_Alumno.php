0c24be19e3e56464205de5f2884e31a253d0581f
Arnold Cruz <arnold_x100pre_10@hotmail.com>
2018-05-03 22:49:40 -0500

    Kardex

---
+++ Reporte_Kardex_Notas_Alumno.php
@@ -0,0 +1,36 @@
+<html>
+<head><title>Kardex de Notas</title></head>
+<body>
+ 
+<h1 aling="center">Listado de Notas </h1>
+ 
+<table with="100%" border="1px" aling="center"> 
+<tr>
+  <td><strong>Nombres</strong></td>
+  <td><strong>Apellidos</strong></td>
+  <td><strong>Curso</strong></td>
+  <td><strong>Nota</strong></td>
+  
+</tr>
+ 
+<tr>
+  <td>Arnold</td>
+  <td>Cruz Carrasco</td>
+  <td>Matematica</td>
+  <td>15</td>
+</tr>
+ 
+<tr>
+  <td>Jostein</td>
+  <td>Huertas Flores</td>
+  <td>Comunicacion</td>
+  <td>13</td>
+</tr>
+ 
+<tr>
+  <td>Frank</td>
+  <td>Mestanza Cabrejo</td>
+  <td>Ingles</td>
+  <td>18</td>
+</tr>
+</table>
